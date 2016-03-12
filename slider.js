6c5162cd94230768f6a6b2ad888b0dd784a98373
manguto2 <manguto2@illinois.edu>
2016-03-12 15:14:00 -0600

    finish ui, ready for integration

---
+++ slider.js
@@ -0,0 +1,15 @@
+
+
+var updateLabel = function(hour) {
+  var displayTime = hour % 12 + 1;
+  var tag = hour > 11? "pm" : "am";
+
+  $('#display-time').text(displayTime + " " + tag);
+}
+
+var slider = $('#slider');
+slider.on("input change", function(){
+    var val = $(this).val(); 
+    updateLabel(val);
+});
+
