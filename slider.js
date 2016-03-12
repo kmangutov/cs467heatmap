701d17daddf1fb53871f015993e69f5c5601c892
manguto2 <manguto2@illinois.edu>
2016-03-12 16:46:23 -0600

    comit

--- slider.js
+++ slider.js
@@ -1,8 +1,11 @@
 
 
 var updateLabel = function(hour) {
-  var displayTime = hour % 12 + 1;
+  var displayTime = hour % 12;
   var tag = hour > 11? "pm" : "am";
+
+  if(hour == 0 || hour == 12)
+    displayTime = 12;
 
   $('#display-time').text(displayTime + " " + tag);
   reloadWithHour(hour);
